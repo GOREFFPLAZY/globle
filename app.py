55a62e45867a2a86043f5bdef5871576a17bdabf
czett <keinaniemand@gmail.com>
2024-09-04 21:21:53 +0200

    leaderboard

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, redirect, session, request
 from bs4 import BeautifulSoup
 import requests, random
+import json
 
 app = Flask(__name__)
 app.secret_key = "gleezeborpglorp"
@@ -36,13 +37,39 @@ def random_city():
 
     return city
 
+def update(name, guesses):
+    with open('leaderboard.json', 'r') as file:
+        data = json.load(file)
+
+    temp = data["leaderboard"]
+    temp.append([name, guesses])
+    data["leaderboard"] = sorted(temp, key=lambda x: x[1])
+
+    with open('leaderboard.json', 'w') as file:
+        json.dump(data, file, indent=4)
+
+def leaderboard():
+    with open('leaderboard.json', 'r') as file:
+        data = json.load(file)
+
+    return data["leaderboard"]
+
 @app.route("/")
 def start():
+    if "name" not in session:
+        return render_template("name.html")
+    else:  
+        return redirect("/country")
+    
+@app.route("/name", methods=["POST"])
+def name():
+    name = request.form["name"]
+    session["name"] = name
     return redirect("/country")
 
 @app.route("/country")
 def country():
-    if "country" in session:
+    if "country" in session and "name" in session:
         pass
     else:
         session["country"] = random_country()
@@ -52,7 +79,7 @@ def country():
 
 @app.route("/city")
 def city():
-    if "city" in session:
+    if "city" in session and "name" in session:
         pass
     else:
         session["city"] = random_city()
@@ -66,6 +93,12 @@ def restart():
 
 @app.route("/pop")
 def pop():
+    session["country"] = None
+    session["city"] = None
+    return redirect("/")
+
+@app.route("/popall")
+def popall():
     session.clear()
     return redirect("/")
 
@@ -83,15 +116,16 @@ def country_play():
 
     if km != "error":
         g = session["guesses"]
+        g.extend([icountry.lower()])
         if icountry.lower() not in g:
-            g.extend([icountry.lower()])
             session["guesses"] = g
         if km == "0":
-            return render_template("country.html", distance=km, guesses=g, won=True)
+            update(session["name"], len(session["guesses"]))
+            return render_template("country.html", distance=km, guesses=g, won=True, leaderboard=leaderboard())
         else:
-            return render_template("country.html", distance=km, guesses=g, won=False)
+            return render_template("country.html", distance=km, guesses=g, won=False, c=session["country"], leaderboard=leaderboard())
     else:
-        return render_template("country.html", distance="NaN", error="Invalid country :(", won=False)
+        return render_template("country.html", distance="NaN", error="Invalid country :(", won=False, leaderboard=leaderboard())
     # except:
     #     return 'vorp'
     #     return redirect("/")
@@ -110,15 +144,16 @@ def city_play():
 
     if km != "error":
         g = session["guesses"]
+        g.extend([icity.lower()])
         if icity.lower() not in g:
-            g.extend([icity.lower()])
             session["guesses"] = g
         if km == "0":
-            return render_template("city.html", distance=km, guesses=g, won=True)
+            update(session["name"], len(session["guesses"]))
+            return render_template("city.html", distance=km, guesses=g, won=True, leaderboard=leaderboard())
         else:
-            return render_template("city.html", distance=km, guesses=g, won=False)
+            return render_template("city.html", distance=km, guesses=g, won=False, leaderboard=leaderboard())
     else:
-        return render_template("city.html", distance="NaN", error="Invalid city :(", won=False)
+        return render_template("city.html", distance="NaN", error="Invalid city :(", won=False, leaderboard=leaderboard())
     # except:
     #     return 'vorp'
     #     return redirect("/")
--- test.py
+++ test.py
@@ -1,3 +1,5 @@
-l = ["a", "b", "c"]
-l.extend(["e"])
-print(l)
+l = [["johanus", 2], ["eike", 1], ["zepeter", 20], ["udo", 5]]
+
+sorted_list = sorted(l, key=lambda x: x[1])
+
+print(sorted_list)
